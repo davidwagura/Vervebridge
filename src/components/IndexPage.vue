ddf8894b4b19ca0cf4e07c26222b9b1d2309eb90
David wanjohi <davidwagura83@gmail.com>
2024-06-04 12:33:03 +0300

    Display response data

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -2,23 +2,27 @@
 
     <div class="border m-40 h-72 pt-8 mt-0 ">
 
-        {{ data }}
-
         <div class="flex justify-center">
 
             <input v-model="city" class="h-10 mr-8 border-gray-500 shadow-xl p-2" placeholder="Enter city" />
 
-            <button class="bg-blue-400 rounded-lg p-2" @click="getWeather()">Get Weather</button> <br>
+            <button class="bg-blue-400 rounded-lg p-2" @click="getWeather">Get Weather</button>
 
         </div>
 
-        <div v-if="weather" class="mt-4 ml-72">
+        <div v-if="loading" class="flex justify-center mt-4">
 
-            <h2 class="m-4 ml-12">City Name: <span class="ml-6">{{ weather.city_name }}</span></h2>
+            <p>Loading...</p>
 
-            <p class="m-4 ml-12">Weather description: <span class="ml-6">{{ weather.weather.description }}</span></p> 
+        </div>
 
-            <p class="m-4 ml-12">Temperature: <span class="ml-6">{{ weather.temp }} °C</span></p> 
+        <div v-if="!loading" class="mt-4 ml-12">
+
+            <h2 v-if="city_name !== ''" class="m-4">City Name: <span class="ml-4">{{ city_name }}</span></h2>
+
+            <p v-if="weather_description !== ''" class="m-4">Weather description: <span class="ml-4">{{ weather_description }}</span></p> 
+
+            <p v-if="temperature !== ''" class="m-4">Temperature: <span class="ml-4">{{ temperature }} °C</span></p> 
 
         </div>
 
@@ -38,9 +42,13 @@ export default {
 
             city: '',
 
-            weather: null,
+            loading: false,
 
-            data: {}
+            city_name: '',
+
+            weather_description: '',
+
+            temperature: ''
 
         };
 
@@ -50,15 +58,28 @@ export default {
 
         async getWeather() {
 
+            this.loading = true;
+
             const API_KEY = '11f996f9c0db43e384a159067612f13c';
 
-            axios.get(`https://api.weatherbit.io/v2.0/current?&city=${this.city}&key=${API_KEY}`)
+            try {
 
-            .then(response => {
+                const response = await axios.get(`https://api.weatherbit.io/v2.0/current?city=${this.city}&key=${API_KEY}`);
 
-                this.data = response.data.data;
+                this.city_name = response.data.data[0].city_name;
 
-            });
+                this.weather_description = response.data.data[0].weather.description;
+
+                this.temperature = response.data.data[0].temp;
+
+            } catch (error) {
+
+                console.error('Error fetching weather data:', error);
+
+            } finally {
+
+                this.loading = false;
+            }
 
         },
 
@@ -67,4 +88,3 @@ export default {
 };
 
 </script>
-  
